a28e7577e5668c0f8a841502254c1b5ea2d730b3
Kirill Verenich <kirillverenih@ya.ru>
2020-08-22 02:14:52 +0300

    delete redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
-import {Provider} from 'react-redux'
-
-import store from './store';
 import './styles/main.scss';
 
 import Main from './views/pages/main';
@@ -11,16 +8,14 @@ import Use from "./views/pages/use";
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/' component={Main}/>
-          <Route exact path='/install' component={Install}/>
-          <Route exact path='/use' component={Use}/>
-          <Route component={() => <Redirect to="/"/>}/>
-        </Switch>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Main}/>
+        <Route exact path='/install' component={Install}/>
+        <Route exact path='/use' component={Use}/>
+        <Route component={() => <Redirect to="/"/>}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/store/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from 'redux';
-
-import imageReducer from "./imageReducer";
-
-export default combineReducers({
-  image: imageReducer,
-});
